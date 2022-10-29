9d1b59e5ab7a8c6235c6db63df78e6767bdb2d89
ddLion247 <115746556+ddLion247@users.noreply.github.com>
2022-10-29 16:21:42 -0400

    Message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [x, setX] = useState(6);
+  const [array, setArray] = useState([]);
+
+  useEffect(()=> {
+    console.log('run useEffect');
+    const tempArray = [];
+    for (let i = 0; i < x; i++) {
+      tempArray.push(i);
+    }
+    setArray(tempArray);
+  }, [x]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={() => setX(4)}>Add</button>
+      </div>
+      {array.map(element => <span>{element} </span>)}
+      
     </div>
   );
 }
