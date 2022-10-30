a3f2d223c0881d8205c378aed6891170aae74c43
Yonathan Dessalene <ydessalene@gmail.com>
2022-10-30 15:00:50 -0400

    carousel

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,15 +1,33 @@
 import { useEffect, useState } from 'react';
 import gyr from './res/gyr.png';
+import chonkers from './res/chonkers.jpg';
+import lizard from './res/air-lizard.png';
+import turtle from './res/land-turtle.jpg';
 import './Carousel.css';
 import React from 'react';
+import { Card } from '@mui/material';
+import WestIcon from '@mui/icons-material/West';
+import EastIcon from '@mui/icons-material/East';
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 
 export class Carousel extends React.Component {
     render() {
+        
+
         return (
             <div className='carousel'>
-                <span><img src={gyr} alt='Not found' className='small-img'></img></span>
-                <span><img src={gyr} alt='Not found' className='big-img'></img></span>
-                <span><img src={gyr} alt='Not found' className='small-img'></img></span>
+                <Card className='card small-card'>
+                    <WestIcon className="icon"/>
+                    <img src={gyr} alt='Not found' className='img'></img>
+                </Card>
+                <Card className='card big-card'>
+                    <PlayCircleIcon className="icon"/>
+                    <img src={chonkers} alt='Not found' className='img'></img>
+                </Card>
+                <Card className='card small-card'>
+                    <EastIcon className="icon"/>
+                    <img src={turtle} alt='Not found' className='img'></img>
+                </Card>
             </div>
         );
     }
