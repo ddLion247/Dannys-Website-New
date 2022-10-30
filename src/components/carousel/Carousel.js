a309781ed666d555d17446534d45d6c93a66162a
Yonathan Dessalene <ydessalene@gmail.com>
2022-10-30 16:13:24 -0400

    carousel

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -9,26 +9,62 @@ import { Card } from '@mui/material';
 import WestIcon from '@mui/icons-material/West';
 import EastIcon from '@mui/icons-material/East';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import PauseCircleIcon from '@mui/icons-material/PauseCircle';
 
-export class Carousel extends React.Component {
-    render() {
-        
+export function Carousel() {
+    const images = [gyr, chonkers, lizard, turtle];
+    const [display, setDisplay] = useState([images[0], images[1], images[2]]);
+    const [displayedNum, setDisplayedNum] = useState([0, 1, 2]);
+    const [play, togglePlay] = useState(false);
 
-        return (
-            <div className='carousel'>
-                <Card className='card small-card'>
-                    <WestIcon className="icon"/>
-                    <img src={gyr} alt='Not found' className='img'></img>
-                </Card>
-                <Card className='card big-card'>
-                    <PlayCircleIcon className="icon"/>
-                    <img src={chonkers} alt='Not found' className='img'></img>
-                </Card>
-                <Card className='card small-card'>
-                    <EastIcon className="icon"/>
-                    <img src={turtle} alt='Not found' className='img'></img>
-                </Card>
-            </div>
-        );
+    function rotateLeft() {
+        const tempArray = [];
+        for (const num of displayedNum) {
+            let tempNum = num - 1;
+            if (tempNum < 0) {
+                tempNum = images.length - 1;
+            }
+            tempArray.push(tempNum);
+        }
+
+        setDisplayedNum(tempArray);
+        setDisplay([images[tempArray[0]], images[tempArray[1]], images[tempArray[2]]]);
     }
+
+    function rotateRight() {
+        const tempArray = [];
+        for (const num of displayedNum) {
+            let tempNum = num + 1;
+            if (tempNum >= images.length) {
+                tempNum = 0;
+            }
+            tempArray.push(tempNum);
+        }
+
+        setDisplayedNum(tempArray);
+        setDisplay([images[tempArray[0]], images[tempArray[1]], images[tempArray[2]]]);
+    }
+
+    function toggleShuffle() {
+        togglePlay(!play);
+
+    }
+
+    return (
+        <div className='carousel'>
+            <Card className='card small-card' onClick={() => rotateLeft()}>
+                <WestIcon className="icon"/>
+                <img src={display[0]} alt='Not found' className='img'></img>
+            </Card>
+            <Card className='card big-card' onClick={() => toggleShuffle()}>
+                {play && <PauseCircleIcon className="icon"/>}
+                {!play && <PlayCircleIcon className="icon"/>}
+                <img src={display[1]} alt='Not found' className='img'></img>
+            </Card>
+            <Card className='card small-card' onClick={() => rotateRight()}>
+                <EastIcon className="icon"/>
+                <img src={display[2]} alt='Not found' className='img'></img>
+            </Card>
+        </div>
+    );
 };
