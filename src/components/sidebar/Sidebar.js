46317a30f35a0f93923da78e6361d60466f69ed5
ddLion247 <115746556+ddLion247@users.noreply.github.com>
2022-12-16 21:50:54 -0500

    changed divs in sidebar to anchors

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -3,43 +3,15 @@ import React from 'react';
 
 export function Sidebar() {
 
-    function navHome() {
-        window.location.href = '/';
-    }
-    function navMakingOfThisWebsite() {
-        window.location.href = '/makingOfThisWebsite';
-    }
-
-    function navFoundational() {
-        window.location.href = '/foundational';
-    }
-
-    function navRecipeCards() {
-        window.location.href = '/recipeCards';
-    }
-
-    function navWebserver() {
-        window.location.href = '/webserver';
-    }
-
-    function navDatabase() {
-        window.location.href = '/database';
-    }
-
-    function navLeaflet() {
-        window.location.href = '/leaflet';
-    }
-
     return (
         <div className='sidebar'>
-            <div className='sidebarItem' onClick={()=> navHome()}>Home</div>
-            <div className='sidebarItem' onClick={()=> navMakingOfThisWebsite()}>Making of this website</div>
-            <div className='sidebarItem' onClick={()=> navFoundational()}>Foundational Projects</div>
-            <div className='sidebarItem' onClick={()=> navRecipeCards()}>Recipe Cards</div>
-            <div className='sidebarItem' onClick={()=> navWebserver()}>Webserver</div>
-            <div className='sidebarItem' onClick={()=> navDatabase()}>Database</div>
-            <div className='sidebarItem' onClick={()=> navLeaflet()}>Leaflet</div>
-            {/* <div className='sidebarItem'>Battleship??</div> */}
+            <a className='sidebarItem' href = '/'>Home</a>
+            <a className='sidebarItem' href = '/makingOfThisWebsite'>Making of this website</a>
+            <a className='sidebarItem' href = '/foundational'>Foundational Projects</a>
+            <a className='sidebarItem' href = '/recipeCards'>Recipe Cards</a>
+            <a className='sidebarItem' href = '/webserver'>Webserver</a>
+            <a className='sidebarItem' href = '/database'>Database</a>
+            <a className='sidebarItem' href = '/leaflet'>Leaflet</a>
         </div>
-    );
+    ); 
 };
